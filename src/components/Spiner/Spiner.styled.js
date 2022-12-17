40e3fb4f4fea1b236aa9786cde36f03b0e5cfdeb
anna-zau <annazauholnikova555@gmail.com>
2022-12-17 12:11:46 +0200

    gallery update

---
+++ src/components/Spiner/Spiner.styled.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgb(240, 255, 255, 0.5);
+`;
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,32 +2,77 @@ import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { Spiner } from '../components/Spiner/Spiner';
 
 export class Gallery extends Component {
   state = {
     query: '',
     page: 1,
-    images: []
-  }
+    images: [],
+    showBtn: false,
+    error: null,
+    loading: false,
+  };
 
   componentDidUpdate = (prevProps, prevState) => {
-    const { page, query } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      ImageService.getImages(query, page).then(data => this.setState(prevState =>({images: [...prevState.images, ...data.photos]})))
-    }
-  }
-  
+    const { page, query, loading } = this.state;
 
-  handleSubmit = (query) => {
-    this.setState({query})
-  }
- 
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({ loading: true });
+      ImageService.getImages(query, page)
+        .then(data =>
+          this.setState(prevState => ({
+            images: [...prevState.images, ...data.photos],
+            showBtn: page < Math.ceil(data.total_results / 15),
+          }))
+        )
+        .catch(error => {
+          this.setState({ error: 'Something went wrong. Please try again' });
+        })
+        .finally(() => {
+          this.setState({ loading: false });
+        });
+    }
+  };
+
+  handleSubmit = query => {
+    this.setState({
+      query,
+      images: [],
+      page: 1,
+    });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
 
   render() {
     return (
       <>
         <SearchForm onSubmit={this.handleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
+        {this.state.loading && <Spiner />}
+        <Grid>
+          {this.state.images.map(({ id, avg_color, src, alt }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
+        {this.state.showBtn && (
+          <Button onClick={this.handleLoadMore}>Load more</Button>
+        )}
+
+        {this.state.error && (
+          <Text textAlign="center">❌ - {this.state.error}</Text>
+        )}
       </>
     );
   }
